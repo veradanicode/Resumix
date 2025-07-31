5621a0fab0e8e4c577d1a1ad3fe25a199544b86e
Vera D <veradanicode@gmail.com>
2025-07-31 12:40:32 +0100

    complete landing page with animated header, styles, and navbar

--- Resumix/my-app/src/components/LandingPage.jsx
+++ Resumix/my-app/src/components/LandingPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './LandingPage.css'
+import stamp from '../assets/stamp.png'
 
 const LandingPage = () => {
   return (
     <div className='container'>
         <div className='content'>
+          <img src={stamp} alt="stamp" />
             <h1 className='header'>RES<span className='u'>U</span>MIX</h1>
             <p>Mix, match, and build your perfect resume in minutes.</p>
             <button> Generate resume</button>
--- Resumix/my-app/src/components/Navbar.jsx
+++ Resumix/my-app/src/components/Navbar.jsx
@@ -3,10 +3,18 @@ import './Navbar.css'
 function Navbar () {
   return (
     <div className='navbar'>
-      <ul>
-        <li>dded</li>
-        <li>edcxedxed</li>
+      <header className='header-with-triangle'>
+           <div className='triangle-background'></div>
+      <nav>
+      <div className='logo'>ResumeMix</div>
+      <ul className='navbar' >
+        <li><a href="#features">Features</a></li>
+        <li><a href="#templates">Templates</a></li>
+        <li><a href="#contact">Contact</a></li>
       </ul>
+      </nav>
+      </header>
+    
     </div>
   )
 }
