e8aa78fd666eddd3f612142a2afc60011f0212ed
Vera D <veradanicode@gmail.com>
2025-08-04 13:01:08 +0100

    add responsive hamburger menu and improve font scaling on small screens

--- Resumix/my-app/src/components/Navbar.jsx
+++ Resumix/my-app/src/components/Navbar.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
   return (
     <nav className="navbar">
       <div className="logo">Resumix</div>
-      <ul className="nav-links">
+
+      <div className="menu-toggle" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+
+      <ul className={`nav-links ${menuOpen ? 'open' : ''}`}>
         <li><a href="#features">Features</a></li>
         <li><a href="#templates">Templates</a></li>
         <li><a href="#how-it-works">How It Works</a></li>
