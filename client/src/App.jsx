f967d6106da9ebcab4a34e8bceeb3991e00ceaba
Vera D <veradanicode@gmail.com>
2025-09-02 16:04:32 +0100

    update commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,11 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import LandingPage from './components/LandingPage';
 import ResumeForm from './components/ResumeForm';
+import Features from './components/features';
+import Templates from './components/Templates';
+import HowItWorks from './components/HowItWorks';
+
+
 
 function App() {
   return (
@@ -10,6 +15,9 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/resume-form" element={<ResumeForm />} />
+        <Route path="/features" element={<Features />} />
+        <Route path="/templates" element={<Templates />} />
+        <Route path="/how-it-works" element={<HowItWorks />} />
       </Routes>
     </>
   );
