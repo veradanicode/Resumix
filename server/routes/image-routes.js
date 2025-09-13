b189ce6e2ec1cb735ae9f69fe0c4a63f080ae18f
Vera D <veradanicode@gmail.com>
2025-09-13 18:34:03 +0100

    complete image upload routing

--- server/routes/image-routes.js
+++ server/routes/image-routes.js
@@ -1,8 +1,9 @@
 const express =require('express');
-
-
+const uploadMiddleware =require('../middlewares/upload-middleware')
+const {ImageUpload} =require('../controllers/image-controllers')
 const router =express.Router();
 
-
+//upload router
+router.post('/upload',uploadMiddleware.single('image'),ImageUpload)
 
 module.exports=router;
