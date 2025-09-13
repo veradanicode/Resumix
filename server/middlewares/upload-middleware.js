bef4d896428987aa0e24d065689d34c516ac8d45
Vera D <veradanicode@gmail.com>
2025-09-13 18:32:04 +0100

    complete upload middleware functionalities for proper filtering and storage of image

---
+++ server/middlewares/upload-middleware.js
@@ -0,0 +1,32 @@
+const multer =require('multer')
+const path =require('path')
+
+//set up multer storage
+const storage =await multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,'uploads/')
+    },
+    filename:function(req,file,cb){
+        cb(null,file.fieldname+"-"+Date.now()+"-"+path.extname(file.originalname))
+    }
+})
+
+//file filter function
+const checkFileFilter =(req,file,cb)=>{
+    if (file.mimetype.startsWith('image')) {
+        cb(null,true)
+    }else{
+        cb(new Error('Not an Image!please upload only images'))
+    }
+}
+
+//multer middleware
+const upload =multer({
+    storage:storage,
+    fileFilter:checkFileFilter,
+    limits:{
+        fileSize:5*1024 *1024 //5mb
+    }
+})
+
+module.exports =upload;
