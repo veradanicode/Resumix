7e803c8b095aaab549c549f2bb3617464c7af23f
Vera D <veradanicode@gmail.com>
2025-09-13 23:32:46 +0100

    fix image upload route

--- server/routes/image-routes.js
+++ server/routes/image-routes.js
@@ -1,9 +1,11 @@
 const express =require('express');
-const uploadMiddleware =require('../middlewares/upload-middleware')
-const {ImageUpload} =require('../controllers/image-controllers')
+const uploadMiddleware =require('../middlewares/upload-middleware');
+const {ImageUpload} =require('../controllers/image-controllers');
 const router =express.Router();
 
 //upload router
-router.post('/upload',uploadMiddleware.single('image'),ImageUpload)
+router.post('/upload',
+    uploadMiddleware.single('image'),
+      ImageUpload)
 
 module.exports=router;
