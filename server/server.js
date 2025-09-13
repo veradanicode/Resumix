00b1f9fc189e8c37d59a330d75dececa7c139c0b
Vera D <veradanicode@gmail.com>
2025-09-13 18:34:42 +0100

    add image upoad route as parent route in server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express =require('express')
 const connectToDB =require('./database/db')
+const uploadImageRoutes =require('./routes/image-routes')
 
 const app =express()
 const PORT=process.env.PORT ||3000
@@ -10,6 +11,9 @@ app.use(express.json())
 
 //connect to database
 connectToDB()
+
+//routes
+app.use('/api/image',uploadImageRoutes)
 
 app.listen(PORT,()=>{
     console.log(`Resumix server is running at port ${PORT}`);
